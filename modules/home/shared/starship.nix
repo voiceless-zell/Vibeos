a0eab168335c9c3034803578f3ce72dbb38ecdf5
Gremlin Prime <gremlin-prime@local.invalid>
2026-04-16 16:57:45 -0400

    starship: use font-safe git prompt symbols

--- modules/home/shared/starship.nix
+++ modules/home/shared/starship.nix
@@ -5,6 +5,23 @@
     package = pkgs.starship;
     settings = {
       scan_timeout = 1000;
+
+      git_branch = {
+        symbol = "git:";
+      };
+
+      git_status = {
+        ahead = "^''${count}";
+        behind = "v''${count}";
+        diverged = "^''${ahead_count}v''${behind_count}";
+        conflicted = "=''${count}";
+        deleted = "x''${count}";
+        modified = "!''${count}";
+        renamed = ">''${count}";
+        staged = "+''${count}";
+        stashed = "*''${count}";
+        untracked = "?''${count}";
+      };
     };
   };
 }
